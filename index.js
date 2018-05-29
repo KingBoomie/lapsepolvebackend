9ddc5c453374db593261fd5b4ae5097b06df54ff
Kristjan Laht <kristjan.laht13@gmail.com>
2018-05-29 22:59:12 +0300

    smart-id

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var clientId = process.env.client_id;
+var clientSecret = process.env.client_secret;
+var redirect_uri = "https://lit-hollows-38242.herokuapp.com";
+
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -25,17 +29,69 @@ app.get('/db', async (req, res) => {
   }
 })
 
-app
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/times', (req, res) => {
-    let result = ""
-    const times = process.env.times || 5
-    for (var i = 0; i < times; i++) {
-      result += i + " "
-      
-    }
-    res.send(result)
-  })
+var oauth2 = require('simple-oauth2')({
+  clientID: clientId,
+  clientSecret: clientSecret,
+  site: 'https://id.smartid.ee',
+  tokenPath: '/oauth/access_token',
+  authorizationPath: '/oauth/authorize'
+});
 
-app
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// Authorization uri definition
+var authorization_uri = oauth2.authCode.authorizeURL({
+  redirect_uri: redirect_uri
+});
+
+
+//Make it possible to show the image
+app.use(express.static('public'));
+
+
+// Initial page redirecting to Smart ID 
+app.get('/auth', function (req, res) {
+
+});
+
+// Callback service parsing the authorization token and asking for the access token
+app.get('/', function (req, res) {
+  var code = req.query.code;
+  var login = req.query.login;
+  console.log(code + ", " + login);
+  if (typeof code === 'undefined' && typeof login !== 'undefined') {
+      res.redirect(authorization_uri);
+      return;
+  } else if (typeof code !== 'undefined') {
+      oauth2.authCode.getToken({
+          code: code,
+          redirect_uri: redirect_uri
+      }, saveToken);
+  } else {
+      var url = req.protocol + '://' + req.get('host')
+      var pageHtml = '<strong>Click the image below to start login</strong><br>'
+              + '<a href="?login=true"><img src="'+url+'/eidas.jpg"></img></a>';
+      res.send(pageHtml);
+  }
+
+
+  function saveToken(error, result) {
+      if (error) {
+          console.log('Access Token Error', error.message);
+      }
+      console.log("Saving token");
+      token = oauth2.accessToken.create(result);
+      request({
+          url: 'https://id.smartid.ee/api/v2/user_data',
+          headers: {
+              "Authorization": "Bearer " + token.token.access_token
+          }
+
+      }, function (err, userResult) {
+          console.log("Got result");
+          console.log(userResult.body);
+          res.send(userResult.body);
+      });
+  }
+
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
