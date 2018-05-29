af64be6f006ad4afffaeeaf10dbe30f46c5b209e
Kristjan Laht <kristjan.laht13@gmail.com>
2018-05-29 20:17:12 +0300

    env var test

--- index.js
+++ index.js
@@ -7,4 +7,13 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/times', (req, res) => {
+    let result = ""
+    const times = process.env.times || -1
+    for (var i = 0; i < times; i++) {
+      result += i + " "
+      
+    }
+    res.send(result)
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
