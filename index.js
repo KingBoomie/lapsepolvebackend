5da3bae02eb33f43eafafb819095aef6f652ae3b
Kristjan Laht <kristjan.laht13@gmail.com>
2018-05-29 22:34:38 +0300

    database

--- index.js
+++ index.js
@@ -2,10 +2,30 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+const app = express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    res.render('pages/db', result);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
+
+app
   .get('/', (req, res) => res.render('pages/index'))
   .get('/times', (req, res) => {
     let result = ""
@@ -16,4 +36,6 @@ express()
     }
     res.send(result)
   })
+
+app
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
