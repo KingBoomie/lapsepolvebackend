a49b913bb43aa586e3bddc326f9b3923ef1af3b1
Kristjan Laht <kristjan.laht13@gmail.com>
2018-05-30 03:10:31 +0300

    fix cors

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const pool = new Pool({
 });
 
 const app = express()
-.use(cors({credentials: true, origin: 'http://90.190.222.228:8000'}))
+.use(cors({credentials: true, origin: 'https://lapsepolvemaagia.netlify.com/'}))
 .use(express.static(path.join(__dirname, 'public')))
 .set('views', path.join(__dirname, 'views'))
 .set('view engine', 'ejs')
