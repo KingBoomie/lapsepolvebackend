1b8e1945ea3036ba658781e4d1078160f8cd85a6
Kristjan Laht <kristjan.laht13@gmail.com>
2018-05-30 11:36:05 +0300

    fingerprinting

--- index.js
+++ index.js
@@ -14,20 +14,33 @@ const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: true
+  //ssl: true
 });
 
+const whitelist = ['http://localhost:5000', 'http://localhost:8000', 'https://lapsepolvemaagia.netlify.com']
+const corsOptions = {
+  credentials: true,
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
 const app = express()
-app.use(cors({credentials: true, origin: 'https://lapsepolvemaagia.netlify.com'}))
+app.use(cors(corsOptions))
 .use(express.static(path.join(__dirname, 'public')))
+.use(express.json())
 .set('views', path.join(__dirname, 'views'))
 .set('view engine', 'ejs')
 
 const server = require('http').Server(app)
 const io = socketio(server)
 
-// set up db listener
 
+// set up db listener
 var pg_client;
 pool.connect()
   .then(client => { client.query('LISTEN newload'); pg_client = client; })
@@ -54,21 +67,9 @@ io.on('connection', (client) => {
   })
 })
 
-const oauth2 = require('simple-oauth2').create({
-  client: {
-    id: clientId, 
-    secret: clientSecret, 
-  },
-  auth: {
-    tokenHost: 'https://id.smartid.ee',
-    tokenPath: '/oauth/access_token',
-  }
-  //authorizationPath: '/oauth/authorize'
-});
 
 
-
-
+// database test
 app.get('/db', async (req, res) => {
   try {
     const client = await pool.connect()
@@ -81,6 +82,17 @@ app.get('/db', async (req, res) => {
   }
 })
 
+const oauth2 = require('simple-oauth2').create({
+  client: {
+    id: clientId, 
+    secret: clientSecret, 
+  },
+  auth: {
+    tokenHost: 'https://id.smartid.ee',
+    tokenPath: '/oauth/access_token',
+  }
+  //authorizationPath: '/oauth/authorize'
+});
 
 
 // Authorization uri definition
@@ -104,7 +116,8 @@ app.get('/auth', function (req, res) {
   var login = req.query.login;
   console.log(code + ", " + login);
   if (typeof code === 'undefined' && typeof login !== 'undefined') {
-      res.redirect(authorization_uri);
+    console.log("auth uri", authorization_uri)  
+    res.redirect(authorization_uri);
       return;
   } else if (typeof code !== 'undefined') {
       oauth2.authorizationCode.getToken({
@@ -139,5 +152,35 @@ app.get('/auth', function (req, res) {
   }
 
 });
+
+app.put('/fingerprint', async (req, res) => {
+  //console.log(req.body)
+  const q_new_user = {
+    text: `INSERT INTO users 
+    (device_hash, user_agent, language, color_depth, pixel_ratio, hardware_concurrency, resolution, available_resolution, timezone_offset, session_storage, local_storage, indexed_db, open_database, cpu_class, navigator_platform, do_not_track, regular_plugins, canvas, webgl, adblock, has_lied_languages, has_lied_resolution, has_lied_os, has_lied_browser, touch_support, js_fonts) 
+    VALUES 
+    ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26)
+    ON CONFLICT DO NOTHING
+    `,
+    values: req.body,
+  }
+  const q_visit = {
+    text: "INSERT INTO visits (device_hash) VALUES ($1)", 
+    values: [req.body[0]],
+  }
+
+  try {
+    const client = await pool.connect()
+    const result_new = await client.query(q_new_user)
+    const result_visit = await client.query(q_visit)
+    res.send({new: result_new.rowCount, visit: result_visit.rowCount});
+    //console.log(result_new)
+    //console.log(result_visit)
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
 
 server.listen(PORT, () => console.log(`Listening on ${ PORT }`))
