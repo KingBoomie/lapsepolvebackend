eca8aa8c5e3105e6d1516f8a9c2cabf5b2f990fa
Kristjan Laht <kristjan.laht13@gmail.com>
2018-05-30 11:36:43 +0300

    db fix

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  //ssl: true
+  ssl: true
 });
 
 const whitelist = ['http://localhost:5000', 'http://localhost:8000', 'https://lapsepolvemaagia.netlify.com']
